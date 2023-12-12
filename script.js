f0ee3cdd1c2b1b3f51788f4dfdb56958a02eebbf
qurbonovozodbek <ozodbekkurbonov185@gmail.com>
2023-12-12 19:00:49 +0500

    masala

---
+++ script.js
@@ -0,0 +1,114 @@
+// 1 - masala
+
+// let all = 1;
+// if (a % 10 == 0) {
+//     all = all *= a;
+// }
+// if (b % 10 == 0) {
+//     all = all *= b;
+// }
+// if (c % 10 == 0) {
+//     all = all *=c;
+// }
+// console.log(all)
+
+
+// 2 - masala
+
+// let a = +prompt("Birinchi sonni kiriting");
+// let b = +prompt("Ikkinchi sonni kiriting");
+// let c = +prompt("Uchinchi sonni kiriting");
+// let d = +prompt("Tortinchi sonni kiriting");
+
+// let all = 1;
+// if (a % 3 == 0 || a % 7 == 0) {
+//     all = all *= a;
+// }
+// if (b % 3 == 0 || b % 7 == 0) {
+//     all = all *= b;
+// }
+// if (c % 3 == 0 || c % 7 == 0) {
+//     all = all *= c;
+// }
+// if (d % 3 == 0 || d % 7 == 0) {
+//     all = all *= d;
+// }
+// console.log(all)
+
+
+// 3 - masala 
+
+// let a = 99;
+// let butun = a % 10;
+// let qoldiq = Math.trunc(a / 10);
+// let result = (qoldiq*butun);
+// console.log(result);
+
+
+// 4 - masala 
+
+// let a = 35;
+// let on = (Math.trunc(a / 10));
+// let max;
+// if (on % 2 == 0) {
+//     max = on ** 2;
+// } else {
+//     max = on ** 3;
+// }
+// console.log(max)
+
+
+// 5 - masala 
+
+// let a = 345;
+// let qoldiq = Math.trunc(a / 10);
+// console.log(qoldiq)
+
+
+// 6 - masala
+
+// let a = 333;
+// let son1 = Math.trunc(a / 100);
+// let ikki = Math.trunc(a % 100);
+// let son2 = Math.trunc(ikki / 10);
+// let son3 = ikki % 10;
+
+// let result = 1;
+// if (son1 % 2 == 1) {
+//     result = result *= son1;
+// }
+// if (son2 % 2 == 1) {
+//     result = result *= son2;
+// }
+// if (son3 % 2 == 1) {
+//     result = result *= son3;
+// }
+
+// console.log(result)
+
+
+// 7 - masala 
+
+// let a = 2222;
+// let son1 = Math.trunc(a / 1000);
+// let uch = Math.trunc(a % 1000);
+// let son2 = Math.trunc(uch / 100);
+// let ikki = uch % 100;
+// let son3 = Math.trunc(ikki / 10);
+// let son4 = ikki % 10;
+
+// let result = 1;
+// if (son1 % 2 == 0) {
+//     result = result *= son1;
+// }
+// if (son2 % 2 == 0) {
+//     result = result *= son2;
+// }
+// if (son3 % 2 == 0) {
+//     result = result *= son3;
+// }
+// if (son4 % 2 == 0) {
+//     result = result *= son4;
+// }
+
+// console.log(result)
